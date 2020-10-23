876ae630cd64790c2c4dba4ade9583533b440076
vsaeide <vsaeide7798@gmail.com>
2020-10-23 12:35:33 +0330

    stemming and delete punctiations

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import csv
+import string
+import nltk
 from hazm import *
 from nltk import word_tokenize
-# nltk.download('punkt')
+from nltk.stem import PorterStemmer
+nltk.download('punkt')
 import xmltodict
 
 
 def clean_Persion_doc(doc):
+    stemmer = Stemmer()
+    lemmatizer = Lemmatizer()
     normalizer = Normalizer()
+
     doc = normalizer.normalize(doc)
-    tokenized = word_tokenize(doc)
-    return tokenized
+    tokenized=word_tokenize(doc)
+    stemmed=[stemmer.stem(w) for w in tokenized]
+    new_words = [word for word in stemmed if word.isalnum()]
+    lemmatized=[lemmatizer.lemmatize(w) for w in new_words]
+
+    return lemmatized
 
 
 with open('Persian.xml') as persionfile:
@@ -29,7 +39,8 @@ with open('Persian.xml') as persionfile:
                 writer1.writerow(clean_text)
                 writer2.writerow(text)
 
-##################
+
+#################
 
 Englishfile = open('ted_talks.csv', 'r', newline='')
 list_data = list(csv.reader(Englishfile))
@@ -38,8 +49,22 @@ Englishfile.close()
 Englishfile = open('ted_talks_modified.csv', 'w', newline='')
 writer = csv.writer(Englishfile)
 writer.writerow(list_data[0])
+counter=0
 for ld in list_data[1:]:
-    ld[1] = str(word_tokenize(ld[1]))
-    ld[14] = str(word_tokenize(ld[14]))
+    tokenizer = nltk.RegexpTokenizer(r"\w+")
+    ps = PorterStemmer()
+
+    ld[1] = tokenizer.tokenize(ld[1])
+    ld[14]= tokenizer.tokenize(ld[14])
+    #stem
+
+    ld[1]=[ps.stem(w) for w in ld[1]]
+    ld[14]=[ps.stem(w) for w in ld[14]]
+
     writer.writerow(ld)
+
+
+    counter+=1
+    if counter==5:
+        break
 Englishfile.close()
